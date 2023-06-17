b6231657496d2037ab24ce80810071e1bfa883b9
NeeRaj-2401 <niraj.yadav@slrtce.in>
2023-06-17 21:06:28 +0530

    [code]: redux state testings

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import { useState, useEffect } from 'react'
-import { fetchDataFromApi } from './utils/api'
+import { useState, useEffect } from 'react';
+import { fetchDataFromApi } from './utils/api';
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiCofiguration, getGenres } from './store/homeSlice';
 
 
 function App() {
 
+  const dispatch = useDispatch()
+  const {url} = useSelector((state) => state.home)
+
   useEffect(
-    ()=>{
+    () => {
       apitesting();
-    }, []  )
+    }, [])
 
   const apitesting = () => {
     fetchDataFromApi('/movie/popular').then(
       (res) => {
         console.log(res);
+        dispatch(getApiCofiguration(res));
       });
   };
 
   return (
     <div className='App'>
-     
+      {url?.total_pages}
     </div>
   )
 }

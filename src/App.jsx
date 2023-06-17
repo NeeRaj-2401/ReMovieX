3af2ad5eb24a6bc51b97f09e17ab29e2053f3d54
NRJ YDV <88180887+NeeRaj-2401@users.noreply.github.com>
2023-06-17 20:34:15 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import { fetchDataFromApi } from './utils/api'
+
+
+function App() {
+
+  useEffect(
+    ()=>{
+      apitesting();
+    }, []  )
+
+  const apitesting = () => {
+    fetchDataFromApi('/movie/popular').then(
+      (res) => {
+        console.log(res);
+      });
+  };
+
+  return (
+    <div className='App'>
+     
+    </div>
+  )
+}
+
+export default App

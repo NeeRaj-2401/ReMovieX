ec3f3b887bddb42379e0fb29e13d36859045e2bc
NeeRaj-2401 <niraj.yadav@slrtce.in>
2023-06-24 17:56:15 +0530

    [add]: home trending setup & switcher

---
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import "./style.scss";
+
+const SwitchTabs = ({ data, onTabChange }) => {
+    const [selectedTab, setSelectedTab] = useState(0);
+    const [left, setLeft] = useState(0);
+
+    const activeTab = (tab, index) => {
+        setLeft(index * 100);
+        setTimeout(() => {
+            setSelectedTab(index);
+        }, 300);
+        onTabChange(tab, index);
+    };
+
+    return (
+        <div className="switchingTabs">
+            <div className="tabItems">
+                {data.map((tab, index) => (
+                    <span
+                        key={index}
+                        className={`tabItem ${
+                            selectedTab === index ? "active" : ""
+                        }`}
+                        onClick={() => activeTab(tab, index)}
+                    >
+                        {tab}
+                    </span>
+                ))}
+                {/* this below style={{ left }} is moving the bg to active tab & its value from left is calculated in active tab function */}
+                <span className="movingBg" style={{ left }} />
+            </div>
+        </div>
+    );
+};
+
+export default SwitchTabs;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeroBanner from './heroBanner/HeroBanner';
+import Trending from './trending/Trending';
 import "./style.scss";
 
 function Home() {
   return (
     <div className='homePage'>
-       <HeroBanner/> 
+      <HeroBanner />
+      <Trending />
     </div>
   )
 }
